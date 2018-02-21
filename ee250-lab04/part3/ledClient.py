5837b56135e23e4f43c9f09876ba27f4f696f5d2
Crew Taylor <crewtayl@usc.edu>
2018-02-20 16:58:25 -0800

    sadfsadf

--- ee250-lab04/part3/ledClient.py
+++ ee250-lab04/part3/ledClient.py
@@ -1,6 +1,32 @@
-import sys
-# By appending the folder of all the GrovePi libraries to the system path here,
-# we are successfully `from grovepi import *`
-sys.path.append('../../Software/Python/')
+# LED Client 
+#
+# This code sends requests to the Raspberry Pi to turn on and
+#off the Grove LED using TCP packets.
 
-from grovepi import *
+
+import socket
+
+import groovepi
+
+def Main():
+    host = '10.0.2.15'
+    port = 5000
+
+    s = socket.socket()
+    s.bind((host,port))
+
+    s.listen(1)
+    c, addr = s.accept()
+    print("Connection from: " + str(addr))
+    while True:
+        data = c.recv(1024).decode('utf-8')
+        if not data:
+            break
+        print("From connected user: " + data)
+        data = data.upper()
+        print("Sending: " + data)
+        c.send(data.encode('utf-8'))
+    c.close()
+
+if __name__ == '__main__':
+    Main()
